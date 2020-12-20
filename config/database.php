b528a68784d4abdd8f573ff1ba285991c8edc8ab
Marvin Ruppert <MarvinRuppert2@gmx.de>
2020-12-20 12:51:43 +0100

    DB Grundlagen

---
+++ config/database.php
@@ -0,0 +1,23 @@
+<?php
+
+$dbName = 'CoronaHealthcare';
+
+$dns = 'mysql:dbname='.$dbName.'host=localhost';
+$user = 'root';
+$pw = '';
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+$db = null;
+
+try
+{
+    $db = new PDO($dns, $user, $pw, $options);
+    
+}
+catch (PDOException $e)
+{
+    die('Database connection failed: ' . $e->getMessage());
+}
--- index.php
+++ index.php
@@ -1,1 +1,7 @@
+<?php
+
+require_once 'config/database.php';
+
+
+?>
 <h3>Das ist der Start von Corona Healthcare</h3>

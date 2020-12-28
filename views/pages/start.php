70ee7c26f1b2fbc41bdac743350fa61c49253860
Katja Fischer <katja.fischer@fh-erfurt.de>
2020-12-28 13:22:06 +0100

    start.html in start.php

---
+++ views/pages/start.php
@@ -0,0 +1,39 @@
+<html>
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <meta charset=UTF-8>
+    <title>CoronaHealthcare</title>
+    <link rel="stylesheet" href="../../assets/styles/style.css">
+</head>
+
+<body>
+    <header>
+        <section class="logo"><img src="../../assets/Logo.png" width="300" height="300" alt="Logo"></section>
+        <h1>Willkommen bei CoronaHealthcare</h1>
+        <div class="symbol"><i class="fa fa-user"></i>
+            <i class="fa fa-shopping-cart"> </i>
+            <!--<i class="glyphicon glyphicon-search"></i></div>-->
+    </header>
+
+    <nav>
+        <a href="index.php?page=Schutzmasken">Schutzmasken</a>
+        <a href="index.php?page=Desinfektionsmittel">Desinfektionsmittel</a>
+        <a href="index.php?page=Hygienepapier">Hygienepapier</a>
+        <a href="index.php?page=Reinigungsmittel">Reinigungsmittel</a>
+        <a href="index.php?page=Hautreinigung">Hautreinigung</a>
+    </nav>
+
+    <section class="info">
+        <article class="shopinformationen">Hier stehen Informationen zum Shop
+            <!--Fabian, schreibst du einen Text?-->
+            <img src="../../assets/platzhalter.png" width="330" height="125" alt="Platzhalter"></article>
+        <article class="news">RSS Feed in PHP einbinden</article>
+
+        <article class="webseiteinformationen">Hier stehen Informationen zur Webseite<img src="../../assets/platzhalter.png" width="330" height="125" alt="Platzhalter"></article>
+    </section>
+</body>
+
+</html>

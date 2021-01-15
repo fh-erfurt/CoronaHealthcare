d744d678b4a7780a072bf262b02c999a0220e89f
fabianseeber <65532036+fabianseeber@users.noreply.github.com>
2021-01-15 10:49:31 +0100

    Inhalte hinzugefügt
    Impressum, FAQ, Firmenbeschreibung, Firmengeschichte geschrieben; Produktbeschreibungen und Bilder in Assets eingefügt, Firmenbilder eingefügt

--- views/pages/help.php
+++ views/pages/help.php
@@ -1,1 +1,15 @@
-HILFE
+FAQ
+Gibt es Lieferengpässe durch die Pandemie?
+Wir arbeiten seit Jahren mit zuverlässigen Lieferanten Hand in Hand und haben ein zuverlässiges Netzwerk für den Zufluss neuer Ware. Die Produktion der eigenen Marken läuft zudem auf Hochtouren. 
+Wieso gibt es keinen Impfstoff bei Ihnen zu erwerben?
+Die aktuellen Impfstoffe haben kühlspezifische Auflagen, die unsere Lieferanten leider zum jetzigen Zeitpunkt nicht erfüllen. Wir stehen mit einem neuen Dienstleister in Kontakt und versuchen schnellstmöglich den Verkauf von Impfstoffen zu ermöglichen. Der Erwerb von Impfstoff wird dann nur Fachpersonal zur Verfügung stehen.
+Haben Sie einen Vor-Ort-Verkauf?
+Einen Vor-Ort-Verkauf bieten wir zum jetzigen Zeitpunkt noch nicht an. Kleinstmengen können über unseren Shop oder telefonisch über den Verkauf erworben werden. Hierfür melden Sie sich gern über die Vermittlung bei unserem Haus.
+Bieten Sie die turnusmäßige Lieferung der Ware an?
+Die turnusmäßige Lieferung ist über unseren Verkauf möglich und auch durch diesen zu aktivieren. Hierfür melden Sie sich gern über die Vermittlung bei unserem Haus.
+Welche Erfahrung haben Sie im Gesundheitswesen und mit dem Handel von Präventivmaterial?
+Unser Haus handelt seit vielen Jahren mit Hygieneartikeln, von zertifizierten Lieferanten und auch aus Eigenproduktion. Unsere Herstellung ist geprüft und durch den TÜV Rheinland zertifiziert. Wir legen großen Wert auf Qualität und überprüfen die Lieferungen unserer Partner in regelmäßigen Abständen durch unser QM.
+Welches Rückgaberecht habe ich?
+Bei unseren Produkten gelten die gesetzlichen Rückgaberechte. 
+In welchem Liefergebiet agieren Sie?
+Wir liefern Deutschlandweit. Verbrauchsmaterialien, die keine besondere Kühlung benötigen, können Weltweit versandt werden.
--- views/pages/start.php
+++ views/pages/start.php
@@ -9,9 +9,21 @@
     <body>    
     <div id="main">
         <article>
-            <p>Hier stehen Informationen zum Shop<img src="assets/platzhalter.png" width="330" height="125" alt="Platzhalter"></p>
-            <p>Hier stehen Informationen zur Webseite<img src="assets/platzhalter.png" width="330" height="125" alt="Platzhalter"></p>
-        </article>
+
+Die Auswahl der wichtigsten Corona-Produkte finden Sie bei CoronaHealthcare.
+Für uns alle kam die Corona Pandemie überraschend und niemand war auf die aktuelle Situation vorbereitet. Das Virus hat alle Länder fest im Griff und es wurden weltweit Maßnahmen zur Eindämmung der Pandemie getroffen. Jeder Bürger ist angehalten sein Möglichstes zu tun und trägt täglich einen Teil dazu bei, gemeinsam durch diese schwere Zeit zu gehen.
+Um Sie vor dem Virus zu schützen und ihnen die bestmögliche Sicherheit gewährleisten zu können, hat sich das Unternehmen CoronaHealthcare dazu verpflichtet die Bürger mit eine angemessenen und qualitativ hochwertigen Schutzausrüstungen zu versorgen. Ob Eigenprodukt oder durch unser Qualitätsmanagement geprüfte Ware – unser Motto: Wir.Gegen.Corona wird von allen Mitarbeitern gelebt und dient ihnen. Zu ihrem Schutz.
+
+
+
+Unternehmensgeschichte
+Die CoronaHealthcare ist ein Unternehmen mit langjähriger Erfahrung bei Produktion und Vertrieb von Hygieneartikeln aller Art. 
+1992, kurz nach der Wende begann der Firmengründer, Arnold Ruppert mit der Erfindung und späteren Eigenproduktion einer stark saugfähigen Damenbinde, in einer Garage in Weimar. Er gründete die Firma „SaugWeg - Damenbinden“. Die damals einzigen Angestellten waren seine Frau, Lisa Fischer und der Hausmeister, Martin Seebär. Schnell wurde klar, dass die Räumlichkeiten zu klein wurden und man mietete sich in eine alte Lagerhalle ein. Mit stattlichen 87 Mitarbeitern fusionierte die SaugWeg GmbH nach 10 Jahren mit einem Kleinstunternehmen aus Apolda und es entstand die GermanHealtcare GmbH. Fortan beschäftigte man sich neben der Produktion von Damenhygieneartikeln ebenfalls mit klinischem Hygienebedarf und erweiterte das Portfolio durch fremdproduzierte Ware. 
+Die GermanHealtcare GmbH wuchs stetig und 2018 übernahm der erstgeborene Sohn von Arnold und Lisa Ruppert die Firma. Marvin Nottrott war als Inhaber erfolgreich und erschloss einen zweiten Standort in Hamburg. Neben diesem entstand in Erfurt mit einem Neubau die Hauptzentrale des Unternehmens. Die Geschäfte führte zukünftig sein Stellvertreter, Joseph Lindner – ein guter Freund der Familie und Unternehmer.
+Mit Aufkommen der Pandemie nannte man sich in CoronaHealtcare um, damit die Identifikation und der Fokus zu 100% auf die Eindämmung der Pandemie gelegt werden können. Jeder sollte wissen, dass CH als Unternehmen hinter seinen Bürgern steht und seine Ressourcen voll ausschöpfen wird.
+
+    
+            </article>
         <aside>RSS Feed in PHP einbinden</aside>
     </div>
 </body>

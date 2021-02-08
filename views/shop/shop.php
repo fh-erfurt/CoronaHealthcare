8a30b6dfaffbaf92657516c8d14fa53b05a7a22c
Katja Fischer <katja.fischer@fh-erfurt.de>
2021-02-08 19:50:00 +0100

    Sidebar erstellt, Produkte angeordnet

--- views/shop/shop.php
+++ views/shop/shop.php
@@ -1,20 +1,42 @@
 <html>
-<p>
-<?php if (isset($currentCategory)):  ?>
-    Aktuelle ausgewählte Kategorie: <?= $currentCategory ?><br><br><br>
-<?php endif; ?>
-Anzahl der Produkte: <?= $amountOfFilteredProducts ?> von <?= $amountOfAllProducts ?>
-    <div>
-        <ul>
-            <?php foreach($products as $key=> $value): ?>
-                <li>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Shop</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+</head>
+
+<body>
+        <div class="sidebar">
+            <p>
+                <?php if (isset($currentCategory)) :  ?>
+                    Aktuelle Kategorie: <br> <?= $currentCategory ?><br>
+                <?php endif; ?></p>
+                <p> Anzahl der Produkte: <br><?= $amountOfFilteredProducts ?> von <?= $amountOfAllProducts ?></p>
+            
+            Kategorie <br>
+            Preis <br>
+            Bewertung <br>
+        </div>
+
+
+
+        <?php foreach ($products as $key => $value) : ?>
+
+            <div class="column">
+                <div class="product-card">
                     Produkt: <?= $products[$key]['name']; ?> <br>
                     Preis: <?= $products[$key]['price']; ?> € <br>
                     Beschreibung: <?= $products[$key]['description']; ?> <br>
-                </li>
-                <hr>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-</p>
+                </div>
+            </div>
+<?php endforeach; ?>
+
+
+
+</body>
+
 </html>

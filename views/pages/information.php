dc925487a4ff26f775d79112f1215ccf0a08356d
fabianseeber <65532036+fabianseeber@users.noreply.github.com>
2021-02-17 21:23:10 +0100

    Information.php ergänzt

--- views/pages/information.php
+++ views/pages/information.php
@@ -25,11 +25,50 @@
                 </p>
 
                 <p>
-                    Es wird angenommen, dass die Pandemie ihren Ursprung im asiatischen Raum hat und bei Feldermäusen anfänglich auftrat, welche das zuerst Virus an den Menschen übertragen hatten.
+                    Es wird angenommen, dass die Pandemie ihren Ursprung im asiatischen Raum hat und bei Feldermäusen anfänglich auftrat, welche das Virus zuerst an den Menschen übertragen hatten.
                     Einen direkten Nachweis, ob die Fledermäuse für die Übertragung verantwortlich sind, oder nur einen weiteren Übertragungswirt darstellten, gibt es jedoch bisher nicht. Die ersten 
                     Menschen haben sich nach derzeitigen Erkenntnissen auf einem Seafood-Markt in der chinesischen Stadt Wuhan infiziert, bei dem auch Wildtiere bzw. Organe von anderen Tieren und Reptilien 
-                    angeboten wurden. Die ersten Infektionen wurden im Dezember 2019 bekannt.
+                    angeboten wurden. Die ersten Infektionen wurden im Dezember 2019 bekannt.<br><br>
+                </p>
 
+                <p>
+                    <strong>Symptome und Diagnostik</strong><br>
+                </p>
+
+                <p>
+                    Die stark variierenden Krankheitsverläufe reichen von harmlosen Verläufen bis hin zu schweren Lungenentzündungen und dem Tod. Die durchschnittliche Inkubationszeit, also die Zeitspanne
+                    zwischen Infektion und dem Auftreten von Symptomen, liegt bei etwa 5 bis 6 Tagen. Ein großes Problem ist die Infektiosität des Infizierten. Diese können bereits 1 bis 2 Tage vor dem ersten Auftreten
+                    von Symptomen andere Personen infizieren. Die Zeitspanne der Infektiosität beträgt bei milden Verläufe etwa 10 Tage, bei schweren (hospitalisierten) Verläufen bis zu 15 Tage und in seltenen
+                    Fällen bis zu 20 Tage.<br>
+                    Circa 80% der Infektionen verlaufen asymptomisch oder mild. Im Verlauf der Krankheit sind dann die häufigsten Symptome (trockener) Husten, Fieber und Schnupfen. Neben einer häufig auftretenden
+                    Beeinträchitigung von Geruchs- und Geschmackssinn, kommen manchmal allgemeine Symptome wie Müdigkeit, Abgeschlagenheit, Kopf- und Gliederschmerzen, Erbrechen, Durchfall und Halsschmerzen.<br>
+                    Etwa 20% der Fälle, besonders ältere Menschen, die durch Vorerkrankungen, wie Herzkreislauferkrankungen, Diabetes, Erkrankungen des Atmungssystems, der Leber, der Nieren, Krebserkrankungen,
+                    geschwächt sind, erkranken an schweren Verläufen. Dies ist ebenso bei Menschen, deren Imunsystem durch diverse Krankheiten geschwächt ist, ebenfalls der Fall.<br>
+                    In diesen Fällen treten dann in der Regel eine beidseitige Lungenentzündung mit hohem Fieber, Atemnot bis hin zum Lungenversagen, in seltenen Fällen eine Sepsis auf.<br><br>
+                </p>
+
+
+                <p>
+                    <strong>Langzeitfolgen einer COVID-19-Erkrankung</strong><br>
+                </p>
+
+                <p>
+                    Durch die Neuartigkeit des Virus sind derzeit keine Definitionen über Langzeitauswirkungen und dahergehende Folgeschäden, hervorgerunfen durch eine COVID-19-Erkrankung, möglich. 
+                    Langzeitfolgen wurden bisher nicht als einheitliches Phänomen betrachtet, jedoch traten diverse Krankheitsbilder auf, die sowohl zeitversetzt, als auch parallel in verschiedenen
+                    Ausprägungstufen auftraten.<br>
+                    Die bisherigen Erkentnisse lassen zum Beispiel organische Langzeitfolgen nach Intensivbehandlungen beobachten. Müdigkeitserscheinungen, Gedächtnisverlust, Merk- und Wortfindungsstörungen
+                    kamen vereinzelt auch nach milden Verläufen zum Vorschein. Über Lnagzeitfolgen nach sehr milden Verläufen ist derzeit wenig dokumentiert und bekannt.<br><br>
+                </p>
+
+                <p>
+                    <strong>Verbreitung des Virus und saisonale Effekte</strong><br>
+                </p>
+
+                <p>
+                    Das Virus verbreitet sich insbesondere durch Tröpfcheninfektion, aber auch durch Aerosolinfektion von Mensch zu Mensch. Die direkte Übertragung durch diese beiden Hauptübertragungswege
+                    von Tieren zum Menschen ist bisher nicht belegt.<br>
+                    Die Verbreitung ist laut den <a href="https://www.rki.de/SharedDocs/FAQ/NCOV2019/gesamt.html" style="color: green">Informationen des Robert Koch Instituts</a> schlechter im Sommer. Durch höhere Temperaturen und die stärke UV-Strahlung ist das Virus in seiner Verbreitung gehemmt.
+                    Diese Saisonalität wurde bereits bei anderen humanen Coronaviren beobachtet.
                 </p>
         </div>
 
